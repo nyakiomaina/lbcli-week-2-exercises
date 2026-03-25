0ec1b5ed9f842927b49d08c45b8cacac1181a020
Nyakio Maina <carolinenyakio21@gmail.com>
2026-03-25 14:32:39 +0300

    fixes

--- submission/01.sh
+++ submission/01.sh
@@ -1,3 +1,5 @@
-# Create a wallet with the name "btrustwallet".
+#!/usr/bin/env bash
+set -e
+
 bitcoin-cli -regtest -rpcport=18332 createwallet "btrustwallet" 2>/dev/null || true
 echo "btrustwallet"
--- submission/02.sh
+++ submission/02.sh
@@ -1,2 +1,5 @@
-bitcoin-cli -regtest loadwallet "btrustwallet" 2>/dev/null || true
+#!/usr/bin/env bash
+set -e
+
+bitcoin-cli -regtest -rpcport=18332 loadwallet "btrustwallet" 2>/dev/null || true
 bitcoin-cli -regtest -rpcport=18332 -rpcwallet=btrustwallet getrawchangeaddress bech32
